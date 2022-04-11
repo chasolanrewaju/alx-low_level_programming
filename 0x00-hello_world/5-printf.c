6545ea38e0d3fbf352b25cb72c229e061bb9f558
charles <olanrewaju.charles2014@gmail.com>
2022-04-11 09:00:26 -0700

    6 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -11,14 +11,14 @@
  *
  *    * Return: 0 if exited properly, non-zero otherwise
  *
- *     */
+ **/
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+     printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+     return (0);
 
 }
